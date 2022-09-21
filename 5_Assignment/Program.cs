1e8c8abbb98af25996e0d5cdf66ba09e5e2a0ee0
BiggerSus69 <sune171205@gmail.com>
2022-09-21 19:07:34 +0200

    Did a little trolling / Lavede Fibonacci

--- 5_Assignment/Program.cs
+++ 5_Assignment/Program.cs
@@ -1,19 +1,79 @@
 ﻿//Un-comment below code and fix the issues
 //Push to github
 
-int RecursiveFibonacci(int first, int second, int count)
-{
-    count--;
-    
-    
-    if(count == 0) {
-    
-    }else{
-        return RecursiveFibonacci(1, 1, 2);
-    }
-}
 
-int lastFibo = RecursiveFibonacci(1,1,5);
-Console.WriteLine("Last fibo number was " + lastFibo);
-Console.ReadLine(); 
+    uint FibNum = 0, FibNum1 = 1, FibNum2,i;
+    uint FibbyMan = 37;
+
+    Console.WriteLine("Do you want to see the first 40 Fibonacci numbers?");
+    Console.ReadLine();
+    Console.WriteLine("Of course you do :)");
+    Console.WriteLine("It goes like this:");
+    Console.Write(FibNum + "," + FibNum1);
+
+    /*
+    Fibonacci koden som jeg kun mildt hakkede fra internettet, jeg vidste godt hvordan koden burde virke, og at den burde være virkelig simpel,
+    men blev forvirret af alt den anden kode rundt om, så ledte efter bekræftelse på mine storhjernede idéer på internettet
+    */
+
+    for(i=0;i<FibbyMan;++i){
+
+        FibNum2 = FibNum1 + FibNum;
+        Console.Write("," + FibNum2);
+        Thread.Sleep(500);
+        FibNum = FibNum1;
+        FibNum1 = FibNum2;
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Did you like the Fibonacci numbers?");
+    Console.ReadLine();
+    Console.WriteLine("Want the same numbers, but in longitude now?");
+    string LongitudeTime = Console.ReadLine();
+
+    if(LongitudeTime == "yes"){
+        FibNum = 0; FibNum1 = 1; FibNum2 = i; //Resetter værdierne for fremtidig brug
+        Console.WriteLine("GREAT! You had no choice anyways");
+        Console.WriteLine("Here's the numbers:");
+        Thread.Sleep(500);
+        Console.WriteLine("0:     " + FibNum);
+        Thread.Sleep(500);
+        Console.WriteLine("1:     " + FibNum1);
+        Thread.Sleep(500);
+        
+        for(i=2;i<FibbyMan;++i){
+            Console.Write(i + ":     ");
+            FibNum2 = FibNum1 + FibNum;
+            Console.WriteLine(FibNum2);
+            Thread.Sleep(500);
+            FibNum = FibNum1;
+            FibNum1 = FibNum2;
+         }
+    }
+    else{
+        Console.WriteLine("Too bad :(");
+        Thread.Sleep(1000);
+        Console.WriteLine("Here's some more numbers! :)");
+        
+        FibNum = 0; FibNum1 = 1; FibNum2 = i; //Resetter værdierne for fremtidig brug igen
+        Console.WriteLine("GREAT! You had no choice anyways");
+        Console.WriteLine("Here's the numbers:");
+        Thread.Sleep(500);
+        Console.WriteLine("0:     " + FibNum);
+        Thread.Sleep(500);
+        Console.WriteLine("1:     " + FibNum1);
+        Thread.Sleep(500);
+        
+        for(i=2;i<FibbyMan;++i){
+            Console.Write(i + ":     ");
+            FibNum2 = FibNum1 + FibNum;
+            Console.WriteLine(FibNum2);
+            Thread.Sleep(500);
+            FibNum = FibNum1;
+            FibNum1 = FibNum2;
+         }
+    }
+
+Console.WriteLine("Thanks for looking at my numbers :D Now go away.");
+
+Console.ReadLine();
 
